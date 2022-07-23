54a00a6dcebe84053e090743bb8630ab0ae8a667
yude <i@yude.jp>
2022-07-24 05:34:51 +0900

    Fix indent

--- app/scheduler.py
+++ app/scheduler.py
@@ -38,22 +38,22 @@ async def run(client):
                 print("[WARN] 次のロール ID のテキストチャンネルが設定されていません!: " + str(reminder.target))
             else:
                 tc = client.get_guild(int(guild_id)).get_channel(int(tc_id))
-            if tc is None:
-                print(
-                    "[WARN] テキストチャンネルの ID `"
-                    + tc_id
-                    + "` に対するスケジュール処理は行われませんでした。ロールまたはテキストチャンネルが存在しません。\n"
-                    + "(上記に表示される tc_id は、空欄である可能性があります。)\n"
-                )
-            else:
-                item_name = database.getItemName(reminder.item_id)
-                item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
-                await tc.send(
-                    "🔔 **"
-                    + item_name
-                    + "** の提出期限は `"
-                    + item_limit
-                    + "` ですが、まだ提出されていないようです。\n"
-                    + "可能な限りの早めの提出をお願いします！"
-                )
-                database.delReminder(reminder.id)
+                if tc is None:
+                    print(
+                        "[WARN] テキストチャンネルの ID `"
+                        + tc_id
+                        + "` に対するスケジュール処理は行われませんでした。ロールまたはテキストチャンネルが存在しません。\n"
+                        + "(上記に表示される tc_id は、空欄である可能性があります。)\n"
+                    )
+                else:
+                    item_name = database.getItemName(reminder.item_id)
+                    item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
+                    await tc.send(
+                        "🔔 **"
+                        + item_name
+                        + "** の提出期限は `"
+                        + item_limit
+                        + "` ですが、まだ提出されていないようです。\n"
+                        + "可能な限りの早めの提出をお願いします！"
+                    )
+                    database.delReminder(reminder.id)
