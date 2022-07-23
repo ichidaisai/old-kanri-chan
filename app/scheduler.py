5ac129a748bfa69104ecf9a746764d0b2fedd021
yude <i@yude.jp>
2022-07-24 05:03:51 +0900

    Fix: won't send notifications if target has already submitted

--- app/scheduler.py
+++ app/scheduler.py
@@ -12,27 +12,44 @@ async def run(client):
     guild_id = ""
 
     for reminder in reminders:
-        tc_id = database.getTc(reminder.target, "chat")
-        if guild_id == "":
-            guild_id = database.getGuild()
+        submit_by_item_id = database.getSubmitList(reminder.item_id, None)
+        already_submitted = []
+        for author in submit_by_item_id:
+            already_submitted.append(submit_by_item_id.author_role)
+        already_submitted = set(list(already_submitted))
 
-        tc = client.get_guild(int(guild_id)).get_channel(int(tc_id))
-        if tc is None:
+        if reminder.target in already_submitted:
             print(
-                "[WARN] テキストチャンネルの ID `"
-                + tc_id
-                + "` に対するスケジュール処理は行われませんでした。ロールまたはテキストチャンネルが存在しません。\n"
-                + "(上記に表示される tc_id は、空欄である可能性があります。)\n"
+                "[INFO] 提出先 ID: "
+                + reminder.item_id
+                + ","
+                + "対象ロール ID: "
+                + reminder.target
+                + " "
+                + "に対する通知は、既に提出済みのため行われませんでした。"
             )
         else:
-            item_name = database.getItemName(reminder.item_id)
-            item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
-            await tc.send(
-                "🔔 **"
-                + item_name
-                + "** の提出期限は `"
-                + item_limit
-                + "` ですが、まだ提出されていないようです。\n"
-                + "可能な限りの早めの提出をお願いします！"
-            )
-            database.delReminder(reminder.id)
+            tc_id = database.getTc(reminder.target, "chat")
+            if guild_id == "":
+                guild_id = database.getGuild()
+
+            tc = client.get_guild(int(guild_id)).get_channel(int(tc_id))
+            if tc is None:
+                print(
+                    "[WARN] テキストチャンネルの ID `"
+                    + tc_id
+                    + "` に対するスケジュール処理は行われませんでした。ロールまたはテキストチャンネルが存在しません。\n"
+                    + "(上記に表示される tc_id は、空欄である可能性があります。)\n"
+                )
+            else:
+                item_name = database.getItemName(reminder.item_id)
+                item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
+                await tc.send(
+                    "🔔 **"
+                    + item_name
+                    + "** の提出期限は `"
+                    + item_limit
+                    + "` ですが、まだ提出されていないようです。\n"
+                    + "可能な限りの早めの提出をお願いします！"
+                )
+                database.delReminder(reminder.id)
