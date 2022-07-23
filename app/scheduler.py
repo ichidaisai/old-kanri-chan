b72ab84a0aab36389a7b3e86cf773ce1b758a155
yude <i@yude.jp>
2022-07-24 04:44:58 +0900

    Add error handling

--- app/scheduler.py
+++ app/scheduler.py
@@ -15,16 +15,24 @@ async def run(client):
         tc_id = database.getTc(reminder.target, "chat")
         if guild_id == "":
             guild_id = database.getGuild()
-            
+
         tc = client.get_guild(int(guild_id)).get_channel(int(tc_id))
-        item_name = database.getItemName(reminder.item_id)
-        item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
-        await tc.send(
-            "🔔 **"
-            + item_name
-            + "** の提出期限は `"
-            + item_limit
-            + "` ですが、まだ提出されていないようです。\n"
-            + "可能な限りの早めの提出をお願いします！"
-        )
-        database.delReminder(reminder.id)
+        if tc is None:
+            print(
+                "[WARN] テキストチャンネルの ID `"
+                + tc_id
+                + "` に対するスケジュール処理は行われませんでした。ロールまたはテキストチャンネルが存在しません。\n"
+                + "(上記に表示される tc_id は、空欄である可能性があります。)\n"
+            )
+        else:
+            item_name = database.getItemName(reminder.item_id)
+            item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
+            await tc.send(
+                "🔔 **"
+                + item_name
+                + "** の提出期限は `"
+                + item_limit
+                + "` ですが、まだ提出されていないようです。\n"
+                + "可能な限りの早めの提出をお願いします！"
+            )
+            database.delReminder(reminder.id)
