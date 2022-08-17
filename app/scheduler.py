677a47a2b05a15c6aab473c24acb99ca7087ee8d
yude <i@yude.jp>
2022-08-17 13:51:16 +0900

    Lint

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,6 +1,7 @@
 # 外部関数
 import datetime
 from datetime import timedelta
+
 # 内部関数
 import asyncio
 import database
@@ -61,11 +62,11 @@ async def run(client):
                         + "可能な限りの早めの提出をお願いします！"
                     )
                     database.delReminder(reminder.id)
-    
 
 
 ### 毎週月曜日の10時に通知する
 mon_noti = 0
+
 
 @tasks.loop(minutes=1)
 async def mon_notification(client):
@@ -75,25 +76,29 @@ async def mon_notification(client):
     # 通知する締切の範囲を示す。
     limit_date = datetime.now() + timedelta(days=7)
     # 月曜10時台に実行する。
-    if date == 0 and dt_now.hour == 10 :
+    if date == 0 and dt_now.hour == 10:
         if dt_now.minute == 0 and mon_noti == 0:
             Mon_notification(limit_date)
             print("通知の送信が完了しました")
             # 複数回実行されないようにする
             dt_now.minute = 1
-        elif mon_noti == 1 and dt_now.minute >= 5: # 5分経過で変数を戻す
+        elif mon_noti == 1 and dt_now.minute >= 5:  # 5分経過で変数を戻す
             dt_now.minute = 0
 
 
-async def Mon_notification(client,limit_date):
+async def Mon_notification(client, limit_date):
     guild_id = ""
-    author_role_list = database.getMemberRoles() # 出店者ロール一覧
+    author_role_list = database.getMemberRoles()  # 出店者ロール一覧
 
     for author_role in author_role_list:
-        mon_noti_list = [] # 締切が1週間以内の提出物IDを格納
-        reminders = database.getReminder(author_role,None) # 指定した DiscordID,提出物ID のリマインダーをすべて返す
+        mon_noti_list = []  # 締切が1週間以内の提出物IDを格納
+        reminders = database.getReminder(
+            author_role, None
+        )  # 指定した DiscordID,提出物ID のリマインダーをすべて返す
         for reminder in reminders:
-            submit_by_item_id = database.getSubmitList(reminder.item_id, author_role) # 提出先の ID と提出者のロール ID から、提出された項目のデータを返す
+            submit_by_item_id = database.getSubmitList(
+                reminder.item_id, author_role
+            )  # 提出先の ID と提出者のロール ID から、提出された項目のデータを返す
             already_submitted = []
             for submit in submit_by_item_id:
                 if submit.author_role is not None:
@@ -112,37 +117,29 @@ async def Mon_notification(client,limit_date):
                     + "に対する通知は、既に提出済みのため行われませんでした。"
                 )
             # 未提出の場合
-            else: 
+            else:
                 # 締切が1週間以内か確認する
                 if reminder.datetime < limit_date:
                     # 配列に提出物IDを追加する
                     mon_noti_list.append(reminder.id)
-        
+
         # 配列を基に、通知する。
         tc_id = database.getTc(reminder.target, "chat")
         if guild_id == "":
-                guild_id = database.getGuild()
+            guild_id = database.getGuild()
         tc = client.get_guild(int(guild_id)).get_channel(int(tc_id))
 
         # 提出物IDの重複を削除する
         set(mon_noti_list)
 
-        if len(mon_noti_list) == 0: # 提出物は無い場合
-            await tc.send(
-                "✅今週が締切の提出物はありません"
-            )
-        else: # 提出物がある場合
+        if len(mon_noti_list) == 0:  # 提出物は無い場合
+            await tc.send("✅今週が締切の提出物はありません")
+        else:  # 提出物がある場合
             # なんとかする
-            await tc.send(
-                "⚠️今週が締切の提出物をお知らせします"
-            )
+            await tc.send("⚠️今週が締切の提出物をお知らせします")
             for item_id in mon_noti_list:
                 item_name = database.getItemName(item_id)
                 item_limit = utils.dtToStr(database.getItemLimit(reminder.item_id))
                 await tc.send(
-                    "** 提出物名："
-                    + item_name
-                    + "**   締切: `"
-                    + item_limit
-                    + "`\n"
+                    "** 提出物名：" + item_name + "**   締切: `" + item_limit + "`\n"
                 )
